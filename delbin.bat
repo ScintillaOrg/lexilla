258c4efd4d899515522eed24f87c2f89eaa3ee53
Neil Hodgson <nyamatongwe@gmail.com>
2000-03-09 01:00:34 +0000

    Initial version.

---
+++ delbin.bat
@@ -0,0 +1,6 @@
+del /S /Q *.obj
+del /S /Q *.o
+del /S /Q *.map
+del /S /Q *.res
+del /S /Q *.exe
+del /S /Q *.dll
---
+++ delcvs.bat
@@ -0,0 +1,7 @@
+rmdir /S /Q CVS
+rmdir /S /Q bin\CVS
+rmdir /S /Q doc\CVS
+rmdir /S /Q gtk\CVS
+rmdir /S /Q include\CVS
+rmdir /S /Q src\CVS
+rmdir /S /Q win32\CVS
