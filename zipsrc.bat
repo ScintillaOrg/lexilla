11542337ae4c2ce6326c4100ad239759aa800cb3
Neil Hodgson <nyamatongwe@gmail.com>
2001-02-09 05:37:43 +0000

    Now removes lib files from zip.

--- zipsrc.bat
+++ zipsrc.bat
@@ -1,4 +1,4 @@
 cd ..
 del/q scintilla.zip
-zip scintilla.zip scintilla\*.* scintilla\*\*.* -x *.o -x *.obj -x *.dll 
+zip scintilla.zip scintilla\*.* scintilla\*\*.* -x *.o -x *.obj -x *.dll -x *.lib
 cd scintilla
