f088079fc11706cbe3bbb929a1f951f352780d96
Neil Hodgson <nyamatongwe@gmail.com>
2001-10-02 01:36:31 +0000

    Reinstated WM_GETTEXT as an alias of SCI_GETTEXT to allow some tools to grab the contents of a Scintilla control.

--- win32/ScintillaWin.cxx
+++ win32/ScintillaWin.cxx
@@ -463,6 +463,7 @@ static unsigned int SciMessageFromEM(unsigned int iMessage) {
 		case WM_CLEAR: return SCI_CLEAR;
 		case WM_COPY: return SCI_COPY;
 		case WM_CUT: return SCI_CUT;
+		case WM_GETTEXT: return SCI_GETTEXT;
 		case WM_GETTEXTLENGTH: return SCI_GETTEXTLENGTH;
 		case WM_PASTE: return SCI_PASTE;
 		case WM_UNDO: return SCI_UNDO;
