1a0dfafe6d9d5afc43289773f9c35cc0db80a6cb
Neil Hodgson <nyamatongwe@gmail.com>
2002-08-07 05:49:19 +0000

    New markers called DOTDOTDOT and ARROWS for emulating Python prompts.

--- include/Scintilla.h
+++ include/Scintilla.h
@@ -109,6 +109,8 @@ typedef sptr_t (*SciFnDirect)(sptr_t ptr, unsigned int iMessage, uptr_t wParam, 
 #define SC_MARK_CIRCLEMINUS 20
 #define SC_MARK_CIRCLEMINUSCONNECTED 21
 #define SC_MARK_BACKGROUND 22
+#define SC_MARK_DOTDOTDOT 23
+#define SC_MARK_ARROWS 24
 #define SC_MARK_CHARACTER 10000
 #define SC_MARKNUM_FOLDEREND 25
 #define SC_MARKNUM_FOLDEROPENMID 26
