a81ed0e7ae121ef240951060f91b882e36f785f1
Neil Hodgson <nyamatongwe@gmail.com>
2005-09-18 10:14:18 +0000

    Fixed const correctness issue found by Visual C++ 2005.

--- src/LexCaml.cxx
+++ src/LexCaml.cxx
@@ -273,7 +273,7 @@ void ColouriseCamlDoc(
 
 		case SCE_CAML_OPERATOR: {
 			// [try to] interpret as [additional] operator char
-			char* o = 0;
+			const char* o = 0;
 			if (iscaml(ch) || isspace(ch)			/* ident or whitespace */
 				|| ((o = strchr(")]};,\'\"`#", ch)) != 0)/* "termination" chars */
 				|| !strchr("!$%&*+-./:<=>?@^|~", ch)/* "operator" chars */) {
