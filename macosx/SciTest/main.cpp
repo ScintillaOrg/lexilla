4266e1b541041e4de3ad3f18d4843f7dbe425a5f
Neil Hodgson <nyamatongwe@gmail.com>
2007-06-12 11:36:37 +0000

    Added some basic colours so can see that styling is being done.

--- macosx/SciTest/main.cpp
+++ macosx/SciTest/main.cpp
@@ -67,6 +67,15 @@ static char *ac_class[] = {
 "555555555555555555"
 };
 
+const char keywords[]="and and_eq asm auto bitand bitor bool break "
+"case catch char class compl const const_cast continue "
+"default delete do double dynamic_cast else enum explicit export extern false float for "
+"friend goto if inline int long mutable namespace new not not_eq "
+"operator or or_eq private protected public "
+"register reinterpret_cast return short signed sizeof static static_cast struct switch "
+"template this throw true try typedef typeid typename union unsigned using "
+"virtual void volatile wchar_t while xor xor_eq";
+
 pascal OSStatus WindowEventHandler(EventHandlerCallRef	inCallRef,
 				   EventRef		inEvent,
 				   void*	        inUserData )
@@ -79,7 +88,7 @@ pascal OSStatus WindowEventHandler(EventHandlerCallRef	inCallRef,
 
 	// If the window is not active, let the standard window handler execute.
 	if ( ! IsWindowActive( window ) ) return eventNotHandledErr;
-	
+
 	const HIViewRef rootView = HIViewGetRoot( window );
 	assert( rootView != NULL );
 
@@ -101,7 +110,7 @@ pascal OSStatus WindowEventHandler(EventHandlerCallRef	inCallRef,
 				return noErr;
 			}
 		}
-	
+
 	return eventNotHandledErr;
 }
 
@@ -109,27 +118,26 @@ int main(int argc, char* argv[])
 {
     IBNibRef 		nibRef;
     WindowRef 		window;
-    
+
     OSStatus		err;
 
     // Create a Nib reference passing the name of the nib file (without the .nib extension)
     // CreateNibReference only searches into the application bundle.
     err = CreateNibReference(CFSTR("main"), &nibRef);
     require_noerr( err, CantGetNibRef );
-    
+
     // Once the nib reference is created, set the menu bar. "MainMenu" is the name of the menu bar
     // object. This name is set in InterfaceBuilder when the nib is created.
     err = SetMenuBarFromNib(nibRef, CFSTR("MenuBar"));
     require_noerr( err, CantSetMenuBar );
-    
-    // Then create a window. "MainWindow" is the name of the window object. This name is set in 
+
+    // Then create a window. "MainWindow" is the name of the window object. This name is set in
     // InterfaceBuilder when the nib is created.
     err = CreateWindowFromNib(nibRef, CFSTR("MainWindow"), &window);
     require_noerr( err, CantCreateWindow );
 
     // We don't need the nib reference anymore.
     DisposeNibReference(nibRef);
-    
 
     HIRect boundsRect;
     // GOOD and BAD methods off embedding into a window.  This is used
@@ -171,6 +179,28 @@ int main(int argc, char* argv[])
 
 	scintilla->WndProc( SCI_SETLEXER, SCLEX_CPP, 0);
 	scintilla->WndProc( SCI_SETSTYLEBITS, 5, 0);
+
+	scintilla->WndProc(SCI_STYLESETFORE, 0, 0x808080);	// White space
+	scintilla->WndProc(SCI_STYLESETFORE, 1, 0x007F00);	// Comment
+	scintilla->WndProc(SCI_STYLESETITALIC, 1, 1);	// Comment
+	scintilla->WndProc(SCI_STYLESETFORE, 2, 0x007F00);	// Line comment
+	scintilla->WndProc(SCI_STYLESETITALIC, 2, 1);	// Line comment
+	scintilla->WndProc(SCI_STYLESETFORE, 3, 0x3F703F);	// Doc comment
+	scintilla->WndProc(SCI_STYLESETITALIC, 3, 1);	// Doc comment
+	scintilla->WndProc(SCI_STYLESETFORE, 4, 0x7F7F00);	// Number
+	scintilla->WndProc(SCI_STYLESETFORE, 5, 0x7F0000);	// Keyword
+	scintilla->WndProc(SCI_STYLESETBOLD, 5, 1);	// Keyword
+	scintilla->WndProc(SCI_STYLESETFORE, 6, 0x7F007F);	// String
+	scintilla->WndProc(SCI_STYLESETFORE, 7, 0x7F007F);	// Character
+	scintilla->WndProc(SCI_STYLESETFORE, 8, 0x804080);	// UUID
+	scintilla->WndProc(SCI_STYLESETFORE, 9, 0x007F7F);	// Preprocessor
+	scintilla->WndProc(SCI_STYLESETFORE,10, 0x000000);	// Operators
+	scintilla->WndProc(SCI_STYLESETBOLD,10, 1);	// Operators
+	scintilla->WndProc(SCI_STYLESETFORE,11, 0x000000);	// Identifiers
+
+
+	scintilla->WndProc(SCI_SETKEYWORDS, 0, (sptr_t)(char *)keywords);	// Keyword
+
 	/*
 	these fail compilation on osx now
 	scintilla->WndProc( SCI_SETPROPERTY, "fold", (long int)"1");
@@ -178,7 +208,7 @@ int main(int argc, char* argv[])
 	scintilla->WndProc( SCI_SETPROPERTY, "fold.comment", (long int)"1");
 	scintilla->WndProc( SCI_SETPROPERTY, "fold.preprocessor", (long int)"1");
 	*/
-	
+
 	scintilla->WndProc( SCI_REGISTERIMAGE, 0, (long int)ac_class);
 
 	scintilla->WndProc( SCI_SETMARGINTYPEN, 0, (long int)SC_MARGIN_NUMBER);
@@ -190,13 +220,13 @@ int main(int argc, char* argv[])
 	scintilla->WndProc( SCI_SETMARGINWIDTHN, 2, (long int)16);
 	//scintilla->WndProc( SCI_SETWRAPMODE, SC_WRAP_WORD, 0);
 	//scintilla->WndProc( SCI_SETWRAPVISUALFLAGS, SC_WRAPVISUALFLAG_END | SC_WRAPVISUALFLAG_START, 0);
-	
+
     // set the size of scintilla to the size of the container
     HIViewSetFrame( sciView, &boundsRect );
 
     // bind the size of scintilla to the size of it's container window
     HIViewSetLayoutInfo(sciView, &kBindToParentLayout);
-    
+
     // setup some event handling
     static const EventTypeSpec kWindowMouseEvents[] =
       {
@@ -205,15 +235,15 @@ int main(int argc, char* argv[])
 
     InstallEventHandler( GetWindowEventTarget( window ), WindowEventHandler,
 			 GetEventTypeCount( kWindowMouseEvents ), kWindowMouseEvents, &sciView, NULL );
-    
+
     // show scintilla
     ShowControl(sciView);
-	
+
     SetAutomaticControlDragTrackingEnabledForWindow(window, true);
-	
+
     // The window was created hidden so show it.
     ShowWindow( window );
-    
+
     // Call the event loop
     RunApplicationEventLoop();
 
