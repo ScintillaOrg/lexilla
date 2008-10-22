24dd3cd7c49a29a03393372faaf89e7d546f6001
Neil Hodgson <nyamatongwe@gmail.com>
2008-10-22 09:15:44 +0000

    Fixes a namespace problem with the new replaceable regex.

--- src/Document.cxx
+++ src/Document.cxx
@@ -1657,8 +1657,16 @@ const char *BuiltinRegex::SubstituteByPosition(Document* doc, const char *text, 
 
 #ifndef SCI_OWNREGEX
 
+#ifdef SCI_NAMESPACE
+namespace Scintilla {
+#endif
+
 RegexSearchBase *CreateRegexSearch(CharClassify *charClassTable) {
 	return new BuiltinRegex(charClassTable);
 }
+
+#ifdef SCI_NAMESPACE
+}
+#endif
 
 #endif
