b8c619c48dc9e0564c30eb6f59182c586877e905
Neil Hodgson <nyamatongwe@gmail.com>
2010-09-04 11:42:59 +1000

    Fix for bug #3058924 wrong highlighting for float literals containing +/-. Tests added for example numeric literals.

--- lexers/LexCPP.cxx
+++ lexers/LexCPP.cxx
@@ -513,7 +513,7 @@ void SCI_METHOD LexerCPP::Lex(unsigned int startPos, int length, int initStyle, 
 				break;
 			case SCE_C_NUMBER:
 				// We accept almost anything because of hex. and number suffixes
-				if (!setWord.Contains(sc.ch)) {
+				if (!(setWord.Contains(sc.ch) || ((sc.ch == '+' || sc.ch == '-') && (sc.chPrev == 'e' || sc.chPrev == 'E')))) {
 					sc.SetState(SCE_C_DEFAULT|activitySet);
 				}
 				break;
