99334f4a7266e6a7dc9a69e8f3557e28220695a6
Neil Hodgson <nyamatongwe@gmail.com>
2010-11-08 09:19:59 +1100

    Feature #3104091. Folding for CASE in SQL. From Jérôme LAFORGE.

--- lexers/LexSQL.cxx
+++ lexers/LexSQL.cxx
@@ -307,7 +307,10 @@ static void FoldSQLDoc(unsigned int startPos, int length, int initStyle,
 			} else {
 				s[j] = '\0';
 			}
-			if ((!foldOnlyBegin) && (strcmp(s, "if") == 0 || strcmp(s, "loop") == 0)) {
+			if ((!foldOnlyBegin) && (
+				strcmp(s, "if") == 0 ||
+				strcmp(s, "loop") == 0 ||
+				strcmp(s, "case") == 0)) {
 				if (endFound) {
 					// ignore
 					endFound = false;
