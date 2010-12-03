6e481bb4e572052ccc9366e69c74e3c6973ec627
Neil Hodgson <nyamatongwe@gmail.com>
2010-12-03 14:20:16 +1100

    Remove styling idle in destructor to avoid crashing when idle called after ScintillaGTK instance destroyed.

--- gtk/ScintillaGTK.cxx
+++ gtk/ScintillaGTK.cxx
@@ -367,6 +367,7 @@ ScintillaGTK::ScintillaGTK(_ScintillaObject *sci_) :
 }
 
 ScintillaGTK::~ScintillaGTK() {
+	g_idle_remove_by_data(this);
 }
 
 void ScintillaGTK::RealizeThis(GtkWidget *widget) {
