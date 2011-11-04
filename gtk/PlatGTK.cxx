9fe32f6ddee4562e01f8104e4d6928f213f917d9
Neil Hodgson <nyamatongwe@gmail.com>
2011-11-04 11:04:30 +1100

    Round to nearest pixels horizontally to avoid background artifacts on OS X. From Mitchell Foral.

--- gtk/PlatGTK.cxx
+++ gtk/PlatGTK.cxx
@@ -665,6 +665,8 @@ void SurfaceImpl::RectangleDraw(PRectangle rc, ColourDesired fore, ColourDesired
 void SurfaceImpl::FillRectangle(PRectangle rc, ColourDesired back) {
 	PenColour(back);
 	if (context && (rc.left < maxCoordinate)) {	// Protect against out of range
+		rc.left = lround(rc.left);
+		rc.right = lround(rc.right);
 		cairo_rectangle(context, rc.left, rc.top,
 	                     rc.right - rc.left, rc.bottom - rc.top);
 		cairo_fill(context);
