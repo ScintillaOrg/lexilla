51399e5e56ec403621b3f9fb438432415f182540
Neil Hodgson <nyamatongwe@gmail.com>
2012-01-23 18:19:52 +1100

    Avoid laying out beyond end of document. Bug #3476637.

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -6701,7 +6701,7 @@ void Editor::SetBraceHighlight(Position pos0, Position pos1, int matchStyle) {
 void Editor::SetAnnotationHeights(int start, int end) {
 	if (vs.annotationVisible) {
 		bool changedHeight = false;
-		for (int line=start; line<end; line++) {
+		for (int line=start; line<end && line<pdoc->LinesTotal(); line++) {
 			int linesWrapped = 1;
 			if (wrapState != eWrapNone) {
 				AutoSurface surface(this);
