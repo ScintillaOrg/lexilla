28b916cf7599cdae47e99f14dd525d195112dc57
Neil Hodgson <nyamatongwe@gmail.com>
2013-04-01 14:25:49 +1100

    Feature [#981]. Added autocompletion order property to allow list to be ordered according to container preference but still allowing selection by typing. From Alpha.

--- src/ScintillaBase.cxx
+++ src/ScintillaBase.cxx
@@ -794,6 +794,13 @@ sptr_t ScintillaBase::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lPara
 	case SCI_AUTOCGETCASEINSENSITIVEBEHAVIOUR:
 		return ac.ignoreCaseBehaviour;
 
+	case SCI_AUTOCSETORDER:
+		ac.autoSort = wParam;
+		break;
+
+	case SCI_AUTOCGETORDER:
+		return ac.autoSort;
+
 	case SCI_USERLISTSHOW:
 		listType = wParam;
 		AutoCompleteStart(0, reinterpret_cast<const char *>(lParam));
