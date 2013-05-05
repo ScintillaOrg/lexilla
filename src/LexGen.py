21174926fe59c1c02c85b25f9280cb459dd283cb
Neil Hodgson <nyamatongwe@gmail.com>
2013-05-05 11:12:51 +1000

    Removing support for Visual C++ 2005 and 6.0.

--- src/LexGen.py
+++ src/LexGen.py
@@ -356,7 +356,6 @@ def RegenerateAll():
 
     Regenerate(root + "scintilla/src/Catalogue.cxx", "//", NATIVE, lexerModules)
     Regenerate(root + "scintilla/win32/scintilla.mak", "#", NATIVE, lexFiles)
-    Regenerate(root + "scintilla/win32/scintilla_vc6.mak", "#", NATIVE, lexFiles)
     if os.path.exists(root + "scite"):
         Regenerate(root + "scite/win32/makefile", "#", NATIVE, propFiles)
         Regenerate(root + "scite/win32/scite.mak", "#", NATIVE, propFiles)
