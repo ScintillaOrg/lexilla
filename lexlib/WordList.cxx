7d52b1868b4137fc27a208b61796f0f6b2fdb632
Neil Hodgson <nyamatongwe@gmail.com>
2013-09-06 09:30:46 +1000

    Be more explicit about the empty case to avoid warnings from Visual C++ analyzer.

--- lexlib/WordList.cxx
+++ lexlib/WordList.cxx
@@ -45,22 +45,24 @@ static char **ArrayFromWordList(char *wordlist, int *len, bool onlyLineEnds = fa
 		prev = curr;
 	}
 	char **keywords = new char *[words + 1];
-	words = 0;
-	prev = '\0';
-	size_t slen = strlen(wordlist);
-	for (size_t k = 0; k < slen; k++) {
-		if (!wordSeparator[static_cast<unsigned char>(wordlist[k])]) {
-			if (!prev) {
-				keywords[words] = &wordlist[k];
-				words++;
+	int wordsStore = 0;
+	const size_t slen = strlen(wordlist);
+	if (words) {
+		prev = '\0';
+		for (size_t k = 0; k < slen; k++) {
+			if (!wordSeparator[static_cast<unsigned char>(wordlist[k])]) {
+				if (!prev) {
+					keywords[wordsStore] = &wordlist[k];
+					wordsStore++;
+				}
+			} else {
+				wordlist[k] = '\0';
 			}
-		} else {
-			wordlist[k] = '\0';
+			prev = wordlist[k];
 		}
-		prev = wordlist[k];
 	}
-	keywords[words] = &wordlist[slen];
-	*len = words;
+	keywords[wordsStore] = &wordlist[slen];
+	*len = wordsStore;
 	return keywords;
 }
 
