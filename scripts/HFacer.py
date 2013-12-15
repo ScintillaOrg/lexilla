813d61b50f6136ed3b730ead0445c2e061fc8f9f
Neil Hodgson <nyamatongwe@gmail.com>
2013-12-16 08:34:07 +1100

    Add code for finding unsued values but leave commented out.

--- scripts/HFacer.py
+++ scripts/HFacer.py
@@ -45,6 +45,12 @@ try:
 	f.ReadFromFile("../include/Scintilla.iface")
 	Regenerate("../include/Scintilla.h", "/* ", printHFile(f))
 	Regenerate("../include/SciLexer.h", "/* ", printLexHFile(f))
-	print("Maximum ID is %s" % max([x for x in f.values if int(x) < 3000]))
+	valueSet = set(int(x) for x in f.values if int(x) < 3000)
+	maximumID = max(valueSet)
+	print("Maximum ID is %d" % maximumID)
+	#~ valuesUnused = sorted(x for x in range(2001,maximumID) if x not in valueSet)
+	#~ print("\nUnused values")
+	#~ for v in valuesUnused:
+		#~ print(v)
 except:
 	raise
