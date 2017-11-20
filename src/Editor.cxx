99d740042c68b6d4ccbeabd08cde066116899be4
mitchell <70453897+orbitalquark@users.noreply.github.com>
2017-11-20 12:24:51 +1100

    Add SCI_GETMOVEEXTENDSSELECTION.

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -7625,6 +7625,8 @@ sptr_t Editor::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam) {
 		default:	// ?!
 			return SC_SEL_STREAM;
 		}
+	case SCI_GETMOVEEXTENDSSELECTION:
+		return sel.MoveExtends();
 	case SCI_GETLINESELSTARTPOSITION:
 	case SCI_GETLINESELENDPOSITION: {
 			SelectionSegment segmentLine(
