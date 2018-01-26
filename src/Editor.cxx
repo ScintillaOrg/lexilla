a45f1f152a3e03dc0e19f3a09064457ef05c5eca
Neil Hodgson <nyamatongwe@gmail.com>
2018-01-26 11:12:10 +1100

    Add documentOption argument to SCI_CREATELOADER.

--- src/Editor.cxx
+++ src/Editor.cxx
@@ -7556,6 +7556,7 @@ sptr_t Editor::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam) {
 	case SCI_CREATEDOCUMENT: {
 			Document *doc = new Document();
 			doc->AddRef();
+			doc->Allocate(static_cast<int>(wParam));
 			return reinterpret_cast<sptr_t>(doc);
 		}
 
