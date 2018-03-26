2a5593d8228e2cecfdadd434e2c70b8db3ada8c4
Neil Hodgson <nyamatongwe@gmail.com>
2018-03-26 14:33:28 +1100

    Update Catch from 1.0 build 47 to 2.0.1 and modify UnitTester.cpp to suit. Use #define for Catch to report memory leaks after main as the previous call to _CrtDumpMemoryLeaks was too early and listed a static allocation as a leak.

--- test/unit/UnitTester.cxx
+++ test/unit/UnitTester.cxx
@@ -1,11 +1,16 @@
 // UnitTester.cpp : Defines the entry point for the console application.
 //
 
+// Catch uses std::uncaught_exception which is deprecated in C++17.
+// This define silences a warning from Visual C++.
+#define _SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING
+
 #include <cstdio>
 #include <cstdarg>
 
 #include "Platform.h"
 
+#define CATCH_CONFIG_WINDOWS_CRTDBG
 #define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
 
@@ -29,10 +34,8 @@ void Platform::DebugPrintf(const char *format, ...) {
 
 }
 
-int main(int argc, char* const argv[]) {
+int main(int argc, char* argv[]) {
 	const int result = Catch::Session().run(argc, argv);
-
-	_CrtDumpMemoryLeaks();
 
 	return result;
 }
