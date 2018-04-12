66618fcb7ba83588e571b423d637cf01a9af6a98
Neil Hodgson <nyamatongwe@gmail.com>
2018-04-13 09:03:51 +1000

    Use 'override' for methods that are overridden. Group some method declarations and definitions for AddRef/Release and PerLine.

--- src/Document.cxx
+++ src/Document.cxx
@@ -124,10 +124,38 @@ Document::~Document() {
 	}
 }
 
+// Increase reference count and return its previous value.
+int Document::AddRef() {
+	return refCount++;
+}
+
+// Decrease reference count and return its previous value.
+// Delete the document if reference count reaches zero.
+int SCI_METHOD Document::Release() {
+	const int curRefCount = --refCount;
+	if (curRefCount == 0)
+		delete this;
+	return curRefCount;
+}
+
 void Document::Init() {
 	for (const std::unique_ptr<PerLine> &pl : perLineData) {
 		if (pl)
 			pl->Init();
+	}
+}
+
+void Document::InsertLine(Sci::Line line) {
+	for (const std::unique_ptr<PerLine> &pl : perLineData) {
+		if (pl)
+			pl->InsertLine(line);
+	}
+}
+
+void Document::RemoveLine(Sci::Line line) {
+	for (const std::unique_ptr<PerLine> &pl : perLineData) {
+		if (pl)
+			pl->RemoveLine(line);
 	}
 }
 
@@ -183,34 +211,6 @@ bool Document::SetLineEndTypesAllowed(int lineEndBitSet_) {
 	} else {
 		return false;
 	}
-}
-
-void Document::InsertLine(Sci::Line line) {
-	for (const std::unique_ptr<PerLine> &pl : perLineData) {
-		if (pl)
-			pl->InsertLine(line);
-	}
-}
-
-void Document::RemoveLine(Sci::Line line) {
-	for (const std::unique_ptr<PerLine> &pl : perLineData) {
-		if (pl)
-			pl->RemoveLine(line);
-	}
-}
-
-// Increase reference count and return its previous value.
-int Document::AddRef() {
-	return refCount++;
-}
-
-// Decrease reference count and return its previous value.
-// Delete the document if reference count reaches zero.
-int SCI_METHOD Document::Release() {
-	const int curRefCount = --refCount;
-	if (curRefCount == 0)
-		delete this;
-	return curRefCount;
 }
 
 void Document::SetSavePoint() {
