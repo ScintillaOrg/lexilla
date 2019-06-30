df9fe9a337152f1c059404efed170ffe6c48f829
Zufu Liu <zufuliu@gmail.com>
2019-07-01 08:22:39 +1000

    Bug [#2115]. Fix undefined behaviour of shifting signed 1 by 31.

--- src/MarginView.cxx
+++ src/MarginView.cxx
@@ -268,7 +268,7 @@ void MarginView::PaintMargin(Surface *surface, Sci::Line topLine, PRectangle rc,
 				const bool firstSubLine = visibleLine == firstVisibleLine;
 				const bool lastSubLine = visibleLine == lastVisibleLine;
 
-				int marks = model.pdoc->GetMark(lineDoc);
+				unsigned int marks = model.pdoc->GetMark(lineDoc);
 				if (!firstSubLine)
 					marks = 0;
 
@@ -285,7 +285,7 @@ void MarginView::PaintMargin(Surface *surface, Sci::Line topLine, PRectangle rc,
 							if (levelNum < levelNextNum) {
 								if (model.pcs->GetExpanded(lineDoc)) {
 									if (levelNum == SC_FOLDLEVELBASE)
-										marks |= 1 << SC_MARKNUM_FOLDEROPEN;
+										marks |= 1U << SC_MARKNUM_FOLDEROPEN;
 									else
 										marks |= 1 << folderOpenMid;
 								} else {
