1d7b6c9b290f7d0854ca5ce281f8ddbc3371368c
Neil Hodgson <nyamatongwe@gmail.com>
2020-02-12 09:01:00 +1100

    Bug [#2156]. Process IME messages that were broken by [983d35]. Move WM_INPUTLANGCHANGE[REQUEST] into IMEMessage.

--- win32/ScintillaWin.cxx
+++ win32/ScintillaWin.cxx
@@ -1580,6 +1580,12 @@ sptr_t ScintillaWin::FocusMessage(unsigned int iMessage, uptr_t wParam, sptr_t) 
 sptr_t ScintillaWin::IMEMessage(unsigned int iMessage, uptr_t wParam, sptr_t lParam) {
 	switch (iMessage) {
 
+	case WM_INPUTLANGCHANGE:
+		return ::DefWindowProc(MainHWND(), iMessage, wParam, lParam);
+
+	case WM_INPUTLANGCHANGEREQUEST:
+		return ::DefWindowProc(MainHWND(), iMessage, wParam, lParam);
+
 	case WM_IME_KEYDOWN: {
 			if (wParam == VK_HANJA) {
 				ToggleHanja();
@@ -1594,7 +1600,7 @@ sptr_t ScintillaWin::IMEMessage(unsigned int iMessage, uptr_t wParam, sptr_t lPa
 			return ::DefWindowProc(MainHWND(), iMessage, wParam, lParam);
 		}
 
-	case WM_IME_STARTCOMPOSITION: 	// dbcs
+	case WM_IME_STARTCOMPOSITION:
 		if (KoreanIME() || imeInteraction == imeInline) {
 			return 0;
 		} else {
@@ -1602,7 +1608,7 @@ sptr_t ScintillaWin::IMEMessage(unsigned int iMessage, uptr_t wParam, sptr_t lPa
 			return ::DefWindowProc(MainHWND(), iMessage, wParam, lParam);
 		}
 
-	case WM_IME_ENDCOMPOSITION: 	// dbcs
+	case WM_IME_ENDCOMPOSITION:
 		ImeEndComposition();
 		return ::DefWindowProc(MainHWND(), iMessage, wParam, lParam);
 
@@ -1905,11 +1911,6 @@ sptr_t ScintillaWin::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam
 		case WM_CONTEXTMENU:
 			return ShowContextMenu(iMessage, wParam, lParam);
 
-		case WM_INPUTLANGCHANGE:
-			return ::DefWindowProc(MainHWND(), iMessage, wParam, lParam);
-		case WM_INPUTLANGCHANGEREQUEST:
-			return ::DefWindowProc(MainHWND(), iMessage, wParam, lParam);
-
 		case WM_ERASEBKGND:
 			return 1;   // Avoid any background erasure as whole window painted.
 
@@ -1937,6 +1938,17 @@ sptr_t ScintillaWin::WndProc(unsigned int iMessage, uptr_t wParam, sptr_t lParam
 
 		case WM_GETTEXT:
 			return GetText(wParam, lParam);
+
+		case WM_INPUTLANGCHANGE:
+		case WM_INPUTLANGCHANGEREQUEST:
+		case WM_IME_KEYDOWN:
+		case WM_IME_REQUEST:
+		case WM_IME_STARTCOMPOSITION:
+		case WM_IME_ENDCOMPOSITION:
+		case WM_IME_COMPOSITION:
+		case WM_IME_SETCONTEXT:
+		case WM_IME_NOTIFY:
+			return IMEMessage(iMessage, wParam, lParam);
 
 		case EM_LINEFROMCHAR:
 		case EM_EXLINEFROMCHAR:
