ef16c442e7ab3c72316d3c2efa309bc851ee422a
Neil Hodgson <nyamatongwe@gmail.com>
2020-06-11 08:52:57 +1000

    Avoid warning for potential nullptr dereference that is not actually possible.

--- win32/ScintillaWin.cxx
+++ win32/ScintillaWin.cxx
@@ -3452,8 +3452,10 @@ LRESULT PASCAL ScintillaWin::CTWndProc(
 					}
 					// If above SUCCEEDED, then pCTRenderTarget not nullptr
 					assert(pCTRenderTarget);
-					surfaceWindow->Init(pCTRenderTarget, hWnd);
-					pCTRenderTarget->BeginDraw();
+					if (pCTRenderTarget) {
+						surfaceWindow->Init(pCTRenderTarget, hWnd);
+						pCTRenderTarget->BeginDraw();
+					}
 #endif
 				}
 				surfaceWindow->SetUnicodeMode(SC_CP_UTF8 == sciThis->ct.codePage);
