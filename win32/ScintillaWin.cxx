52c95d9c010b59c2cd79300c36b47195ab0c2dd4
uhf7 <uhf7@localhost>
2020-08-03 13:16:18 +1000

    Bug [#2170]. Fixed cursor flicker when click past end of file and there is an indicator at file end.

--- win32/ScintillaWin.cxx
+++ win32/ScintillaWin.cxx
@@ -1375,7 +1375,10 @@ Window::Cursor ScintillaWin::ContextCursor(Point pt) {
 		} else if (PointIsHotspot(pt)) {
 			return Window::cursorHand;
 		} else if (hoverIndicatorPos != Sci::invalidPosition) {
-			return Window::cursorHand;
+			const Sci::Position pos = PositionFromLocation(pt, true, true);
+			if (pos != Sci::invalidPosition) {
+				return Window::cursorHand;
+			}
 		}
 	}
 	return Window::cursorText;
