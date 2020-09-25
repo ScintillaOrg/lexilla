ce49a3a0567c4e7146ee9630b78341cc25cd809e
Neil Hodgson <nyamatongwe@gmail.com>
2020-09-25 18:00:39 +1000

    Remove Scintilla lines from iface file. Change HFacer.py to only make SciLexer.h.

--- scripts/HFacer.py
+++ scripts/HFacer.py
@@ -4,7 +4,10 @@
 # Implemented 2000 by Neil Hodgson neilh@scintilla.org
 # Requires Python 3.6 or later
 
-import pathlib
+import os, pathlib, sys
+
+sys.path.append(os.path.join("..", "..", "scintilla", "scripts"))
+
 import Face
 
 from FileGenerator import UpdateFile, Generate, Regenerate, UpdateLineInFile, lineEnd
@@ -18,43 +21,10 @@ def printLexHFile(f):
 				out.append("#define " + name + " " + v["Value"])
 	return out
 
-def printHFile(f):
-	out = []
-	previousCategory = ""
-	anyProvisional = False
-	for name in f.order:
-		v = f.features[name]
-		if v["Category"] != "Deprecated":
-			if v["Category"] == "Provisional" and previousCategory != "Provisional":
-				out.append("#ifndef SCI_DISABLE_PROVISIONAL")
-				anyProvisional = True
-			previousCategory = v["Category"]
-			if v["FeatureType"] in ["fun", "get", "set"]:
-				featureDefineName = "SCI_" + name.upper()
-				out.append("#define " + featureDefineName + " " + v["Value"])
-			elif v["FeatureType"] in ["evt"]:
-				featureDefineName = "SCN_" + name.upper()
-				out.append("#define " + featureDefineName + " " + v["Value"])
-			elif v["FeatureType"] in ["val"]:
-				if not ("SCE_" in name or "SCLEX_" in name):
-					out.append("#define " + name + " " + v["Value"])
-	if anyProvisional:
-		out.append("#endif")
-	return out
-
 def RegenerateAll(root, showMaxID):
 	f = Face.Face()
-	f.ReadFromFile(root / "include/Scintilla.iface")
-	Regenerate(root / "include/Scintilla.h", "/* ", printHFile(f))
+	f.ReadFromFile(root / "include/LexicalStyles.iface")
 	Regenerate(root / "include/SciLexer.h", "/* ", printLexHFile(f))
-	if showMaxID:
-		valueSet = set(int(x) for x in f.values if int(x) < 3000)
-		maximumID = max(valueSet)
-		print("Maximum ID is %d" % maximumID)
-		#~ valuesUnused = sorted(x for x in range(2001,maximumID) if x not in valueSet)
-		#~ print("\nUnused values")
-		#~ for v in valuesUnused:
-			#~ print(v)
 
 if __name__ == "__main__":
 	RegenerateAll(pathlib.Path(__file__).resolve().parent.parent, True)
