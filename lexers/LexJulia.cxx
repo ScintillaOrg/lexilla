dedaa7bfde1e1ac590fac074783bc7b1b1721b08
getzze <getzze@gmail.com>
2021-06-25 00:43:43 +0100

    simplify library imports

--- lexers/LexJulia.cxx
+++ lexers/LexJulia.cxx
@@ -10,12 +10,9 @@
 // Copyright 1998-2001 by Neil Hodgson <neilh@scintilla.org>
 // The License.txt file describes the conditions under which this software may be distributed.
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdarg.h>
-#include <assert.h>
-#include <ctype.h>
+#include <cstdlib>
+#include <cassert>
+#include <cstring>
 
 #include <string>
 #include <string_view>
