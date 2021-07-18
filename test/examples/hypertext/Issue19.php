cda350d62f6108f79aaf3621706705c542036456
ivan-u7n <ivan-u7n@users.noreply.github.com>
2021-07-18 12:21:08 +1000

    #19 Support flexible heredoc and nowdoc syntax.

--- test/examples/hypertext/Issue19.php
+++ test/examples/hypertext/Issue19.php
@@ -1,5 +1,24 @@
 <?php
 
 $number = 1_234;
+$var = 'variable value';
+$test = [
+	<<<EOTEST
+	string $var string EOTEST
+	EOTEST_NOT
+	EOTEST,
+	0b00_01,
+	<<<"EOTEST"
+	"string" "$var" "string" EOTEST
+	EOTEST_NOT
+	EOTEST,
+	0x00_02,
+	<<<'EOTEST'
+	'string' '$var' 'string' EOTEST
+	EOTEST_NOT
+	EOTEST,
+	0x00_03,
+];
+print_r($test);
 
 ?>
