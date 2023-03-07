4e397dfd4f6a8f332359b9ba71f6489079df45f5
zeltop <xyz3lt@gmail.com>
2023-03-07 16:38:58 +0100

    #145 #146  Added $ and % highlighting for SCE_GD_NODEPATH in GDScript

--- lexers/LexGDScript.cxx
+++ lexers/LexGDScript.cxx
@@ -94,9 +94,29 @@ int GetGDStringState(Accessor &styler, Sci_Position i, Sci_PositionU *nextIndex)
 	}
 }
 
+int GetGDStringState(int ch) {
+	if (ch != '"' && ch != '\'')
+		return SCE_GD_DEFAULT;
+
+	if (ch == '"')
+		return SCE_GD_STRING;
+	else
+		return SCE_GD_CHARACTER;
+}
+
 inline bool IsAWordChar(int ch, bool unicodeIdentifiers) {
 	if (IsASCII(ch))
 		return (IsAlphaNumeric(ch) || ch == '.' || ch == '_');
+
+	if (!unicodeIdentifiers)
+		return false;
+
+	return IsXidContinue(ch);
+}
+
+inline bool IsANodePathChar(int ch, bool unicodeIdentifiers) {
+	if (IsASCII(ch))
+		return (IsAlphaNumeric(ch) || ch == '_' || ch == '/' || ch =='%');
 
 	if (!unicodeIdentifiers)
 		return false;
@@ -209,6 +229,7 @@ LexicalClass lexicalClasses[] = {
 	13, "SCE_GD_STRINGEOL", "error literal string", "End of line where string is not closed",
 	14, "SCE_GD_WORD2", "identifier", "Highlighted identifiers",
 	15, "SCE_GD_ANNOTATION", "annotation", "Annotations",
+	16, "SCE_GD_NODEPATH", "path", "Node path",
 };
 
 }
@@ -390,7 +411,9 @@ void SCI_METHOD LexerGDScript::Lex(Sci_PositionU startPos, Sci_Position length, 
 	bool indentGood = true;
 	Sci_Position startIndicator = sc.currentPos;
 	bool inContinuedString = false;
-
+	bool percentIsOperator = false;
+	int nodePathStringState = SCE_GD_DEFAULT;
+	
 	for (; sc.More(); sc.Forward()) {
 
 		if (sc.atLineStart) {
@@ -412,6 +435,7 @@ void SCI_METHOD LexerGDScript::Lex(Sci_PositionU startPos, Sci_Position length, 
 		}
 
 		if (sc.atLineEnd) {
+			percentIsOperator = false;
 			ProcessLineEnd(sc, inContinuedString);
 			lineCurrent++;
 			if (!sc.More())
@@ -479,6 +503,18 @@ void SCI_METHOD LexerGDScript::Lex(Sci_PositionU startPos, Sci_Position length, 
 			if (!IsAWordStart(sc.ch, options.unicodeIdentifiers)) {
 				sc.SetState(SCE_GD_DEFAULT);
 			}
+		} else if (sc.state == SCE_GD_NODEPATH) {
+			if (nodePathStringState != SCE_GD_DEFAULT) {
+				if (sc.ch == GetGDStringQuoteChar(nodePathStringState) ) {
+					nodePathStringState = SCE_GD_DEFAULT;
+				}
+			} else {
+				if (IsGDStringStart(sc.ch)) {
+					nodePathStringState = GetGDStringState(sc.ch);
+				} else if (!IsANodePathChar(sc.ch, options.unicodeIdentifiers)) {
+					sc.SetState(SCE_GD_DEFAULT);
+				}
+			}
 		} else if (IsGDSingleQuoteStringState(sc.state)) {
 			if (sc.ch == '\\') {
 				if ((sc.chNext == '\r') && (sc.GetRelative(2) == '\n')) {
@@ -541,6 +577,7 @@ void SCI_METHOD LexerGDScript::Lex(Sci_PositionU startPos, Sci_Position length, 
 						base_n_number = true;
 						sc.SetState(SCE_GD_NUMBER);
 					} else {
+						percentIsOperator = true;
 						sc.SetState(SCE_GD_NUMBER);
 						sc.ForwardSetState(SCE_GD_IDENTIFIER);
 					}
@@ -548,7 +585,11 @@ void SCI_METHOD LexerGDScript::Lex(Sci_PositionU startPos, Sci_Position length, 
 					base_n_number = false;
 					sc.SetState(SCE_GD_NUMBER);
 				}
+			} else if ((sc.ch == '$') || (sc.ch == '%' && !percentIsOperator)) {
+				percentIsOperator = true;
+				sc.SetState(SCE_GD_NODEPATH);
 			} else if (isoperator(sc.ch) || sc.ch == '`') {
+				percentIsOperator = (sc.ch == ')') || (sc.ch == ']') || (sc.ch == '}');
 				sc.SetState(SCE_GD_OPERATOR);
 			} else if (sc.ch == '#') {
 				sc.SetState(sc.chNext == '#' ? SCE_GD_COMMENTBLOCK : SCE_GD_COMMENTLINE);
@@ -564,6 +605,7 @@ void SCI_METHOD LexerGDScript::Lex(Sci_PositionU startPos, Sci_Position length, 
 					sc.Forward();
 				}
             } else if (IsAWordStart(sc.ch, options.unicodeIdentifiers)) {
+				percentIsOperator = true;
 				sc.SetState(SCE_GD_IDENTIFIER);
 			}
 		}
---
+++ test/examples/gdscript/NodePath.gd
@@ -0,0 +1,27 @@
+# nodepath
+
+$Node
+
+%Node
+
+%node/"n o d e"/%'n o d e'
+
+%"No de"
+
+
+$/root/ThisNode/%Node % %test
+
+$MainMenuPanel/%Options % %test
+
+%Options % %test
+
+$Node % %test
+
+
+get_node("%Options") % %test
+
+$"Nod   se" % %test
+
+$/test/"No % de"/test % %test
+
+%node/"n o d e"/'n o d e' % %"No De"
