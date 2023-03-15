9a7c2ac509a135710b80dcffbcf2f5c2bd06e15a
Neil Hodgson <nyamatongwe@gmail.com>
2023-03-16 10:59:10 +1100

    Allow 2-digit main version numbers.

--- scripts/LexillaData.py
+++ scripts/LexillaData.py
@@ -201,8 +201,8 @@ class LexillaData:
     def __init__(self, scintillaRoot):
         # Discover version information
         self.version = (scintillaRoot / "version.txt").read_text().strip()
-        self.versionDotted = self.version[0] + '.' + self.version[1] + '.' + \
-            self.version[2]
+        self.versionDotted = self.version[0:-2] + '.' + self.version[-2] + '.' + \
+            self.version[-1]
         self.versionCommad = self.versionDotted.replace(".", ", ") + ', 0'
 
         with (scintillaRoot / "doc" / "Lexilla.html").open() as f:
