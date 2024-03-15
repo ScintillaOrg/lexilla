a86f23628da5f7b6dcaf190ae8a73efb331fa828
Neil Hodgson <nyamatongwe@gmail.com>
2024-03-16 08:15:21 +1100

    #229 Add COBOL tests.

---
+++ test/examples/cobol/AllStyles.cob
@@ -0,0 +1,36 @@
+      *     Enumerate all styles: 0, 2 to 11, 16
+      *     SCE_C_COMMENTLINE=2
+
+      *     SCE_C_DEFAULT=0
+             
+
+      *     SCE_C_IDENTIFIER=11
+            identifier
+
+      *     SCE_C_NUMBER=4
+            4
+
+      *     SCE_C_WORD=5
+            data
+
+      *     SCE_C_WORD2=16
+            cancel
+
+      *     SCE_C_UUID=8
+            remarks
+
+      *     SCE_C_COMMENTDOC=3 not implemented
+      *     SCE_C_COMMENTDOC has "**" at line start
+      *     but changes style between \r and \n so can't include as test
+
+      *     SCE_C_STRING=6
+            "string"
+
+      *     SCE_C_CHARACTER=7
+            'c'
+
+      *     SCE_C_PREPROCESSOR=9
+?preprocessor
+
+      *     SCE_C_OPERATOR=10
+            +
