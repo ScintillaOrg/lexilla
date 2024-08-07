fcd7c19dea8fbfdd00a4f384026c8ffacead12ad
Neil Hodgson <nyamatongwe@gmail.com>
2024-08-07 15:00:42 +1000

    #259 Force lower-casing of case-insensitive keyword lists so keywords match.

--- lexers/LexAsm.cxx
+++ lexers/LexAsm.cxx
@@ -236,7 +236,7 @@ Sci_Position SCI_METHOD LexerAsm::WordListSet(int n, const char *wl) {
 	}
 	Sci_Position firstModification = -1;
 	if (wordListN) {
-		if (wordListN->Set(wl)) {
+		if (wordListN->Set(wl, true)) {
 			firstModification = 0;
 		}
 	}
