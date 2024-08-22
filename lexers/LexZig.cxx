70d42889241f91b210f1759b37c6e7458ad199e5
Jiří Techet <techet@gmail.com>
2024-08-22 21:24:39 +0200

    Backport Zig lexer from Notepad4
    This patch adds the Zig lexer from the Notepad4 editor originally
    created by Zufu Liu (@zufuliu).
    
    Some changes have been made to make it compile and work in Scintilla
    since Notepad4 contains a modified Scintilla version.
    Also, some features of the Notepad4 lexer have been removed/changed:
    
    - special highlighting of formatting strings has been removed as
      it uses some extra functions added to Notepad4's Scintilla and I didn't
      want to spend much time on figuring out what would have to be
      back-ported - other lexilla lexers don't do this either anyway
    - folding has been modified to use simple folding like the rest of
      Scintilla lexers and not folding of the previous line based on brace
      presence on the next line like Notepad4
    - "semi-syntactic" coloring of Notepad4 which colors functions following
      "fn" has been removed as this is not performed in other Scintilla lexers
    - highlighting of tasks such as TODOs in comments has been removed as it
      isn't present in other Scintilla lexers
    - colorig states and keywords have been renamed slightly - keywords at
      index 3 have been reserved for caller-supplied type names using
      SCI_SETKEYWORDS like in the C lexer (which we use in Geany to supply
      type names obtained using ctags)
    
    Fixes #237.

---
+++ lexers/LexZig.cxx
@@ -0,0 +1,302 @@
+// Scintilla source code edit control
+/** @file LexZig.cxx
+ ** Lexer for Zig language.
+ **/
+// Based on Zufu Liu's Notepad4 Zig lexer
+// Modified for Scintilla by Jiri Techet, 2024
+// The License.txt file describes the conditions under which this software may be distributed.
+
+#include <cassert>
+#include <cstring>
+
+#include <algorithm>
+#include <string>
+#include <string_view>
+
+#include "ILexer.h"
+#include "Scintilla.h"
+#include "SciLexer.h"
+
+#include "WordList.h"
+#include "LexAccessor.h"
+#include "Accessor.h"
+#include "StyleContext.h"
+#include "CharacterSet.h"
+#include "LexerModule.h"
+
+using namespace Lexilla;
+
+namespace {
+// Use an unnamed namespace to protect the functions and classes from name conflicts
+
+constexpr bool IsAGraphic(int ch) noexcept {
+	// excludes C0 control characters and whitespace
+	return ch > 32 && ch < 127;
+}
+
+constexpr bool IsIdentifierStart(int ch) noexcept {
+	return IsUpperOrLowerCase(ch) || ch == '_';
+}
+
+constexpr bool IsIdentifierStartEx(int ch) noexcept {
+	return IsIdentifierStart(ch) || ch >= 0x80;
+}
+
+constexpr bool IsNumberStart(int ch, int chNext) noexcept {
+	return IsADigit(ch) || (ch == '.' && IsADigit(chNext));
+}
+
+constexpr bool IsIdentifierChar(int ch) noexcept {
+	return IsAlphaNumeric(ch) || ch == '_';
+}
+
+constexpr bool IsNumberContinue(int chPrev, int ch, int chNext) noexcept {
+	return ((ch == '+' || ch == '-') && (chPrev == 'e' || chPrev == 'E'))
+		|| (ch == '.' && chNext != '.');
+}
+
+constexpr bool IsDecimalNumber(int chPrev, int ch, int chNext) noexcept {
+	return IsIdentifierChar(ch) || IsNumberContinue(chPrev, ch, chNext);
+}
+
+constexpr bool IsIdentifierCharEx(int ch) noexcept {
+	return IsIdentifierChar(ch) || ch >= 0x80;
+}
+
+// https://ziglang.org/documentation/master/#Escape-Sequences
+struct EscapeSequence {
+	int outerState = SCE_ZIG_DEFAULT;
+	int digitsLeft = 0;
+	bool brace = false;
+
+	// highlight any character as escape sequence.
+	void resetEscapeState(int state, int chNext) noexcept {
+		outerState = state;
+		digitsLeft = 1;
+		brace = false;
+		if (chNext == 'x') {
+			digitsLeft = 3;
+		} else if (chNext == 'u') {
+			digitsLeft = 5;
+		}
+	}
+	void resetEscapeState(int state) noexcept {
+		outerState = state;
+		digitsLeft = 1;
+		brace = false;
+	}
+	bool atEscapeEnd(int ch) noexcept {
+		--digitsLeft;
+		return digitsLeft <= 0 || !IsAHeXDigit(ch);
+	}
+};
+
+enum {
+	ZigLineStateMaskLineComment = 1, // line comment
+	ZigLineStateMaskMultilineString = 1 << 1, // multiline string
+};
+
+enum {
+	KeywordIndex_Primary = 0,
+	KeywordIndex_Secondary = 1,
+	KeywordIndex_Tertiary = 2,
+	KeywordIndex_Type = 3,
+};
+
+const char *const zigWordListDesc[] = {
+	"Primary keywords",
+	"Secondary keywords",
+	"Tertiary keywords",
+	"Global type definitions",
+	nullptr
+};
+
+void ColouriseZigDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle, WordList *keywordLists[], Accessor &styler) {
+	int visibleChars = 0;
+	int lineState = 0;
+	EscapeSequence escSeq;
+
+	StyleContext sc(startPos, lengthDoc, initStyle, styler);
+
+	while (sc.More()) {
+		switch (sc.state) {
+		case SCE_ZIG_OPERATOR:
+			sc.SetState(SCE_ZIG_DEFAULT);
+			break;
+
+		case SCE_ZIG_NUMBER:
+			if (!IsDecimalNumber(sc.chPrev, sc.ch, sc.chNext)) {
+				sc.SetState(SCE_ZIG_DEFAULT);
+			}
+			break;
+
+		case SCE_ZIG_IDENTIFIER:
+		case SCE_ZIG_BUILTIN_FUNCTION:
+			if (!IsIdentifierCharEx(sc.ch)) {
+				if (sc.state == SCE_ZIG_IDENTIFIER) {
+					char s[128];
+					sc.GetCurrent(s, sizeof(s));
+					if (keywordLists[KeywordIndex_Primary]->InList(s)) {
+						sc.ChangeState(SCE_ZIG_KW_PRIMARY);
+					} else if (keywordLists[KeywordIndex_Secondary]->InList(s)) {
+						sc.ChangeState(SCE_ZIG_KW_SECONDARY);
+					} else if (keywordLists[KeywordIndex_Tertiary]->InList(s)) {
+						sc.ChangeState(SCE_ZIG_KW_TERTIARY);
+					} else if (keywordLists[KeywordIndex_Type]->InList(s)) {
+						sc.ChangeState(SCE_ZIG_KW_TYPE);
+					}
+				}
+				sc.SetState(SCE_ZIG_DEFAULT);
+			}
+			break;
+
+		case SCE_ZIG_CHARACTER:
+		case SCE_ZIG_STRING:
+		case SCE_ZIG_MULTISTRING:
+			if (sc.atLineStart) {
+				sc.SetState(SCE_ZIG_DEFAULT);
+			} else if (sc.ch == '\\' && sc.state != SCE_ZIG_MULTISTRING) {
+				escSeq.resetEscapeState(sc.state, sc.chNext);
+				sc.SetState(SCE_ZIG_ESCAPECHAR);
+				sc.Forward();
+				if (sc.Match('u', '{')) {
+					escSeq.brace = true;
+					sc.Forward();
+				}
+			} else if ((sc.ch == '\'' && sc.state == SCE_ZIG_CHARACTER) || (sc.ch == '\"' && sc.state == SCE_ZIG_STRING)) {
+				sc.ForwardSetState(SCE_ZIG_DEFAULT);
+			} else if (sc.state != SCE_ZIG_CHARACTER) {
+				if (sc.ch == '{' || sc.ch == '}') {
+					if (sc.ch == sc.chNext) {
+						escSeq.resetEscapeState(sc.state);
+						sc.SetState(SCE_ZIG_ESCAPECHAR);
+						sc.Forward();
+                    }
+                }
+			}
+			break;
+
+		case SCE_ZIG_ESCAPECHAR:
+			if (escSeq.atEscapeEnd(sc.ch)) {
+				if (escSeq.brace && sc.ch == '}') {
+					sc.Forward();
+				}
+				sc.SetState(escSeq.outerState);
+				continue;
+			}
+			break;
+
+		case SCE_ZIG_COMMENTLINE:
+		case SCE_ZIG_COMMENTLINEDOC:
+			if (sc.atLineStart) {
+				sc.SetState(SCE_ZIG_DEFAULT);
+			}
+			break;
+		}
+
+		if (sc.state == SCE_ZIG_DEFAULT) {
+			if (sc.Match('/', '/')) {
+				if (visibleChars == 0) {
+					lineState = ZigLineStateMaskLineComment;
+				}
+				sc.SetState(SCE_ZIG_COMMENTLINE);
+				sc.Forward();
+				if (sc.chNext == '!' || sc.chNext == '/') {
+					sc.ChangeState(SCE_ZIG_COMMENTLINEDOC);
+				}
+			} else if (sc.Match('\\', '\\')) {
+				lineState = ZigLineStateMaskMultilineString;
+				sc.SetState(SCE_ZIG_MULTISTRING);
+			} else if (sc.ch == '\"') {
+				sc.SetState(SCE_ZIG_STRING);
+			} else if (sc.ch == '\'') {
+				sc.SetState(SCE_ZIG_CHARACTER);
+			} else if (IsNumberStart(sc.ch, sc.chNext)) {
+				sc.SetState(SCE_ZIG_NUMBER);
+			} else if ((sc.ch == '@' && IsIdentifierStartEx(sc.chNext)) || IsIdentifierStartEx(sc.ch)) {
+				sc.SetState((sc.ch == '@') ? SCE_ZIG_BUILTIN_FUNCTION : SCE_ZIG_IDENTIFIER);
+			} else if (IsAGraphic(sc.ch)) {
+				sc.SetState(SCE_ZIG_OPERATOR);
+			}
+		}
+
+		if (visibleChars == 0 && !isspacechar(sc.ch)) {
+			visibleChars++;
+		}
+		if (sc.atLineEnd) {
+			styler.SetLineState(sc.currentLine, lineState);
+			lineState = 0;
+			visibleChars = 0;
+		}
+		sc.Forward();
+	}
+
+	sc.Complete();
+}
+
+struct FoldLineState {
+	int lineComment;
+	int multilineString;
+	constexpr explicit FoldLineState(int lineState) noexcept:
+		lineComment(lineState & ZigLineStateMaskLineComment),
+		multilineString((lineState >> 1) & 1) {
+	}
+};
+
+void FoldZigDoc(Sci_PositionU startPos, Sci_Position lengthDoc, int initStyle, WordList *[] /*keywordLists*/, Accessor &styler) {
+	const Sci_PositionU endPos = startPos + lengthDoc;
+	Sci_Position lineCurrent = styler.GetLine(startPos);
+	FoldLineState foldPrev(0);
+	int levelCurrent = SC_FOLDLEVELBASE;
+	if (lineCurrent > 0) {
+		levelCurrent = styler.LevelAt(lineCurrent - 1) >> 16;
+		foldPrev = FoldLineState(styler.GetLineState(lineCurrent - 1));
+	}
+
+	int levelNext = levelCurrent;
+	FoldLineState foldCurrent(styler.GetLineState(lineCurrent));
+	Sci_PositionU lineStartNext = styler.LineStart(lineCurrent + 1);
+	lineStartNext = std::min(lineStartNext, endPos);
+
+	while (startPos < endPos) {
+		initStyle = styler.StyleAt(startPos);
+
+		if (initStyle == SCE_ZIG_OPERATOR) {
+			const char ch = styler[startPos];
+			if (ch == '{' || ch == '[' || ch == '(') {
+				levelNext++;
+			} else if (ch == '}' || ch == ']' || ch == ')') {
+				levelNext--;
+			}
+		}
+
+		++startPos;
+		if (startPos == lineStartNext) {
+			const FoldLineState foldNext(styler.GetLineState(lineCurrent + 1));
+			levelNext = std::max(levelNext, SC_FOLDLEVELBASE);
+			if (foldCurrent.lineComment) {
+				levelNext += foldNext.lineComment - foldPrev.lineComment;
+			} else if (foldCurrent.multilineString) {
+				levelNext += foldNext.multilineString - foldPrev.multilineString;
+			}
+
+			const int levelUse = levelCurrent;
+			int lev = levelUse | (levelNext << 16);
+			if (levelUse < levelNext) {
+				lev |= SC_FOLDLEVELHEADERFLAG;
+			}
+			styler.SetLevel(lineCurrent, lev);
+
+			lineCurrent++;
+			lineStartNext = styler.LineStart(lineCurrent + 1);
+			lineStartNext = std::min(lineStartNext, endPos);
+			levelCurrent = levelNext;
+			foldPrev = foldCurrent;
+			foldCurrent = foldNext;
+		}
+	}
+}
+
+}  // unnamed namespace end
+
+extern const LexerModule lmZig(SCLEX_ZIG, ColouriseZigDoc, "zig", FoldZigDoc, zigWordListDesc);
